cb22f88b4d1c07b42357b0e94b46fde4033ebaab
Adarsh Kurudi <adarsh.kurudi@hotelsathome.com>
2019-10-15 16:48:23 -0400

    Added helmet with page title.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./utils/theme"
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,12 +9,17 @@ import FormMain from "./components/formMain";
 
 const App = () => {
   return (
+    <>
+    <Helmet>
+      <title>HTML Wrapper</title>
+    </Helmet>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Layout header={<Header />} footer={null}>
         <FormMain />
       </Layout>
     </ThemeProvider>
+    </>
   );
 };
 
