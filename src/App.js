524e14e4ba19dfa356f061bf6d3d9584dfd3587a
Adarsh Kurudi <adarsh.kurudi@hotelsathome.com>
2019-10-15 17:01:41 -0400

    Updated yarn.lock file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {Helmet} from "react-helmet";
 import { ThemeProvider } from "@material-ui/styles";
-import theme from "./utils/theme"
+import theme from "./utils/theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Layout from "./components/layout";
 import Header from "./components/header";
@@ -9,17 +8,12 @@ import FormMain from "./components/formMain";
 
 const App = () => {
   return (
-    <>
-    <Helmet>
-      <title>HTML Wrapper</title>
-    </Helmet>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Layout header={<Header />} footer={null}>
         <FormMain />
       </Layout>
     </ThemeProvider>
-    </>
   );
 };
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import PageTitle from "./pageTitle"
 
 export default props => {
   return (
     <Container>
+      <PageTitle title="HTML Wrapper" />
       <header>{props.header}</header>
       {props.children}
       <footer>{props.footer}</footer>
